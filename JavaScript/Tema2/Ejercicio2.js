6c2470d5a650b81949733ea61c8a17cc3e2c5c31
rosh <hikarimegumi343@gmail.com>
2022-04-18 23:27:23 -0400

    Ejercicio numero 2 del curso de JavasCript.

---
+++ JavaScript/Tema2/Ejercicio2.js
@@ -0,0 +1,16 @@
+"use strict";
+/**
+En este ejercicio tienes que crear un archivo JavaScript donde crees una variable la cual contenga una cadena de texto.
+Después crearás una función, dentro de ella crearas una variable que se llame igual que la primera que has creado y le asignaras una cadena de texto diferente a la primera y mostrarás la variable por consola.
+Por último, fuera de la función tendrás que realizar la llamada a la función y mostrarás por consola la primera variable que has creado.
+*/
+let text = "Hola soy una cadena de texto";
+
+function respuesta() {
+  var text = "Hola, yo soy Roger!";
+  console.log(text);
+}
+// llamada a la funcion
+respuesta();
+// Imprimir en consola primera variable text...
+console.log(text);
