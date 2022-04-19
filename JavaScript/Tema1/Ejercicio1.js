c07b0e46a2d76dc56baaeb2d62c0d717c5d54aae
rosh <hikarimegumi343@gmail.com>
2022-04-18 21:49:37 -0400

    Primer Ejercicio JavaScript, clase 1

---
+++ JavaScript/Tema1/Ejercicio1.js
@@ -0,0 +1,5 @@
+"use strict"
+// Crea un archivo JavaScript donde muestres por consola la siguiente frase: 
+// “Estoy realizando el curso de JavaScript, espero aprender mucho”.
+
+console.log("Estoy realizando el curso de JavaScript, espero aprender mucho");
