847b4e90b76a09bdf661e4b1e8ce6f153419666f
rosh <hikarimegumi343@gmail.com>
2022-05-03 00:31:44 -0400

    Ejercicio 2 de la seccion 4 y reformatting de carpetas

---
+++ JavaScript/Sección4/Ejercicio2.js
@@ -0,0 +1,8 @@
+// Ejercicio2
+//Como segundo ejercicio, tienes que crear otra variable con una cadena de texto y convertir todo el contenido de la cadena a mayúsculas y lo mostraréis por consola.
+
+let str = "El futuro es algo que cada cual alcanza a un ritmo de sesenta minutos por hora, haga lo que haga y sea quien sea."
+console.log(str)
+
+let strMayusculas = str.toUpperCase()
+console.log(strMayusculas)
