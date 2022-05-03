f86fa4500f09e52fba6f950486482a562a17992b
rosh <hikarimegumi343@gmail.com>
2022-05-03 00:01:25 -0400

    Ejercicio1 de la seccion 4

---
+++ JavaScript/Sección4/Ejercicio1.js
@@ -0,0 +1,9 @@
+// En este primer ejercicio tienes que crear una cadena de texto con una frase, 
+// por ejemplo: “El perro de mi vecina es muy molesto” y reemplazar la palabra vecina por vecino. 
+// Por consola tienes que mostrar la frase con las palabras cambiadas.
+
+let miCadena = "Un día llegarás a ser tan maduro que volverás a leer cuentos"
+console.log(miCadena)
+
+let miCadenaCambio = miCadena.replace("maduro", "madura")
+console.log(miCadenaCambio);
